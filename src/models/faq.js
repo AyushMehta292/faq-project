c8497377c28b78c4d44a006c371b320d3c4374f1
Ayush_Mehta <2021ugee076@nitjsr.ac.in>
2025-02-02 15:36:54 +0530

    feat: Add the faq model schema

---
+++ src/models/faq.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const { translateText } = require('../services/translationService');
+
+const FAQSchema = new mongoose.Schema({
+  question: { type: String, required: true },
+  answer: { type: String, required: true },
+  question_hi: { type: String, default: '' },
+  question_bn: { type: String, default: '' },
+  answer_hi: { type: String, default: '' },
+  answer_bn: { type: String, default: '' },
+  created_at: { type: Date, default: Date.now }
+});
+
+FAQSchema.methods.getTranslation = function (lang) {
+  if (lang === 'hi') {
+    return {
+      question: this.question_hi || this.question,
+      answer: this.answer_hi || this.answer
+    };
+  } else if (lang === 'bn') {
+    return {
+      question: this.question_bn || this.question,
+      answer: this.answer_bn || this.answer
+    };
+  }
+  return {
+    question: this.question,
+    answer: this.answer
+  };
+};
+
+FAQSchema.pre('save', async function (next) {
+  try {
+    if (!this.question_hi) {
+      this.question_hi = await translateText(this.question, 'hi');
+    }
+    if (!this.question_bn) {
+      this.question_bn = await translateText(this.question, 'bn');
+    }
+    if (!this.answer_hi) {
+      this.answer_hi = await translateText(this.answer, 'hi');
+    }
+    if (!this.answer_bn) {
+      this.answer_bn = await translateText(this.answer, 'bn');
+    }
+    next();
+  } catch (error) {
+    console.error('Pre-save translation error:', error);
+    next(error);
+  }
+});
+
+const FAQ = mongoose.model('FAQ', FAQSchema);
+module.exports = FAQ;
